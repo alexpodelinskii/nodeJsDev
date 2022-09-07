694e7ad0ae4d18e7996065aed032d31e2b763396
alexpodelinskii <nikitik00787@gmail.com>
2022-09-07 18:10:32 +0300

    Module FS

--- app.js
+++ app.js
@@ -87,14 +87,32 @@ http
 
 /// NOdemon
 
-const http = require('http')
+// const http = require('http')
 
-let message = 'Hello World!'
-http
-    .createServer(function (request, response) {
-        console.log(message)
-        response.end(message)
-    })
-    .listen(3000, '127.0.0.1', () => {
-        console.log('Сервер начал прослушивание запросов')
-    })
+// let message = 'Hello World!'
+// http
+//     .createServer(function (request, response) {
+//         console.log(message)
+//         response.end(message)
+//     })
+//     .listen(3000, '127.0.0.1', () => {
+//         console.log('Сервер начал прослушивание запросов')
+//     })
+
+//**************************************************** */
+
+//Работа с файлами
+
+const fs = require('fs');
+
+const path = './files/hello.txt'
+
+const fileContent = fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data);
+    }
+
+})
+
